5ecfca47154ececc5ac4b8fcd18b914d4568a6c2
Sofia Mattiasson <mattiasson.sofia@icloud.com>
2023-08-23 11:47:23 +0200

    Add folder stracture to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/App.scss'
 
 const App = () => {
+  return (
+    <>
+      <h1>TMDB</h1>
+    </>
+  );
+};
 
-    return (
-        <>
-            <h1>TMDB</h1>
-        </>
-    )
-}
-
-export default App
+export default App;
