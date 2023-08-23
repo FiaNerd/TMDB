6c0c3a0cd06a729e54282915e4c9d9be4a4d217b
Sofia Mattiasson <mattiasson.sofia@icloud.com>
2023-08-23 18:45:40 +0200

    Add hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./partial/Header"
 import "./assets/App.scss"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
 	return (
 		<>
 			<Header />
+			<HomePage />
 		</>
 	)
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import { Button } from "react-bootstrap"
+
+const Hero = () => {
+	return (
+		<div className="hero-container p-5 text-center bg-image">
+			<div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+				<div className="d-flex justify-content-center align-items-center h-100">
+					<div className="text-white">
+						<h1 className="mb-3">Heading</h1>
+						<h4 className="mb-3">Subheading</h4>
+
+						<Button className="btn btn-outline-light btn-lg">
+							Call to action
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
