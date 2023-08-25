b6ec08712033898208d629569ff61fe2d2242bb2
Sofia Mattiasson <mattiasson.sofia@icloud.com>
2023-08-25 18:26:18 +0200

    Add responsive footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,9 @@ const Hero = () => {
 	return (
 		<div className="hero-container p-5 text-center bg-image">
 			<div className="hero-mask ">
-				<div className="text-white d-flex justify-content-center align-items-center h-100">
-					<div className="text-white p-2">
-						<h1 className="mb-3 mt-5 ">Your Movie Journey Begins Here</h1>
+				<div className=" d-flex justify-content-center align-items-center h-100">
+					<div className="text-container text-white p-2">
+						<h1 className="mb-3 mt-5 ">YOUR MOVIE JOURNEY BEGINS HERE</h1>
 						<h4 className="mb-5">
 							Whether you love horror, action, or romance, there's a movie
 							waiting for you.
--- src/partial/Footer.tsx
+++ src/partial/Footer.tsx
@@ -2,14 +2,21 @@ import { Button } from "react-bootstrap"
 
 const Footer = () => {
 	return (
-		<footer className="footer-container">
-			<div className="footer-wrapper">
-				<p className="text-footer">
-					In the dim glow, cinematic dreams materialize, igniting emotions that
-					only theaters conjure
-				</p>
-				<Button className="btn-footer">ENJOY A MOVIE</Button>
+		<footer>
+			<div className="footer-container">
+				<div className="footer-wrapper">
+					<img
+						src="../../images/logoTMDB.svg"
+						alt="logo image"
+						className="logo-footer"
+					/>
+					<p className="text-footer">
+						IN THE DIM GLOW, CINEMATIC DREAMS MATERAALIZE, IGNITING EMOTIONS
+						THAT ONLY THEATERS CONJURE
+					</p>
+				</div>
 			</div>
+			<Button className="btn-footer">ENJOY A MOVIE</Button>
 			<div className="footer-copy">
 				<p className="text-copy">&copy; 2023 TMDB | All Rights Reserved.</p>
 			</div>
